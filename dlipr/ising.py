890fb9101aa5780f90660b29454ec8e81ab1c9c5
DavidWalz <walz@physik.rwth-aachen.de>
2017-05-26 22:41:09 +0200

    update ising

--- dlipr/ising.py
+++ dlipr/ising.py
@@ -5,7 +5,6 @@ See https://arxiv.org/abs/1605.01735
 
 from dlipr.utils import get_datapath, Dataset
 import numpy as np
-import pickle
 
 
 def load_data():
@@ -14,26 +13,20 @@ def load_data():
     Returns:
         Dataset: Ising data
     """
+    data = np.load(get_datapath('Ising/data.npz'))
+    X = data['C']
+    y = data['T']
 
-    def load_pickle(fname):
-        with open(fname, 'rb') as handle:
-            return pickle.load(handle)
+    temperatures = np.arange(1, 3.51, 0.1)
+    y = np.searchsorted(temperatures, y)
 
-    fname = get_datapath('Ising/batch_%i.pickle')
-    X_train = np.empty((50000, 1024))
-    y_train = np.empty((50000))
-
-    for i in range(5):
-        X, y = load_pickle(fname % (i + 1))
-        X_train[i * 10000: (i + 1) * 10000] = X
-        y_train[i * 10000: (i + 1) * 10000] = y
-
-    X_test, y_test = load_pickle(fname % 5)
+    X_train, X_test = np.split(X, [22000])
+    y_train, y_test = np.split(y, [22000])
 
     data = Dataset()
+    data.classes = temperatures
     data.train_images = X_train
     data.train_labels = y_train
     data.test_images = X_test
     data.test_labels = y_test
-    data.classes = np.arange(0.2, 5.1, 0.2)  # temperature
     return data
