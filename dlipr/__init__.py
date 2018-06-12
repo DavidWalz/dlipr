ee14333aeab2825e8de385ca46231f8543e435dc
JGlombitza <jonas.glombitza@rwth-aachen.de>
2018-06-12 10:19:45 +0200

    change to 2 jobs per gpu setup

--- dlipr/__init__.py
+++ dlipr/__init__.py
@@ -14,3 +14,14 @@ import dlipr.svhn
 import dlipr.ising
 import dlipr.flower
 import dlipr.speckles
+import tensorflow as tf
+default_init = tf.Session.__init__
+
+mem_frac = 0.45
+
+def init_with_gpu_frac(self, *args, **kwargs):
+    kwargs['config'] = tf.ConfigProto(gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=mem_frac))
+    default_init(self, *args, **kwargs)
+
+
+tf.Session.__init__ = init_with_gpu_frac
